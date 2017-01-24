a084d664039b2f74c6eee96de6591df4b3fa8ade
Andrey Chepurenko <alan6161@yandex.ru>
2017-01-24 22:13:24 +0300

    some models

---
+++ RehearsalPortal.Domain/Base.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RehearsalPortal.Domain
+{
+    class Base
+    {
+        string Id { get; set; }
+
+        string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public IEnumerable<string> Phones { get; set; }
+
+        public string City { get; set; }
+
+        //public string Pic { get; set; }
+    }
+}
---
+++ RehearsalPortal.Domain/BaseUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RehearsalPortal.Domain
+{
+    class BaseUser
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<string>[] Phones { get; set; }
+
+        //public string Pic { get; set; }
+    }
+}
