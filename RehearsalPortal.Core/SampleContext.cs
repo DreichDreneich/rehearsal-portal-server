2728ca6013121285fa3ac61e849b1298ef75a478
Andrey Chepurenko <alan6161@yandex.ru>
2017-01-24 21:59:46 +0300

    simple entity framework integration

---
+++ RehearsalPortal.Core/SampleContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using RehearsalPortal.Domain;
+
+namespace RehearsalPortal.Core
+{
+    public class UserContext : DbContext
+    {
+        public UserContext()
+            : base("DbConnection")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- src/RehearsalPortalServer/Controllers/ValuesController.cs
+++ src/RehearsalPortalServer/Controllers/ValuesController.cs
@@ -3,17 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RehearsalPortal.Core;
+using RehearsalPortal.Domain;
 
-namespace RehearsalPortalServer.Controllers
+namespace RehearsalPortal.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<User> Get()
         {
-            return new string[] { "value1", "value2" };
+            using (UserContext db = new UserContext())
+            {
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Sam", Age = 26 };
+
+                // добавляем их в бд
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Users;
+
+                return users.ToList();
+            }
         }
 
         // GET api/values/5
