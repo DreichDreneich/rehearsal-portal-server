47a4307ce0f47fd85a8fd1f4c01b958a99240275
Andrey Chepurenko <alan6161@yandex.ru>
2017-01-24 22:45:17 +0300

    add seed

--- RehearsalPortal.Core/SampleContext.cs
+++ RehearsalPortal.Core/SampleContext.cs
@@ -7,8 +7,18 @@ namespace RehearsalPortal.Core
     {
         public UserContext()
             : base("DbConnection")
-        { }
+        {
+            Database.SetInitializer(new DBInitializer());
+        }
 
         public DbSet<User> Users { get; set; }
+    }
+
+    public class DBInitializer : DropCreateDatabaseAlways<UserContext>
+    {
+        protected override void Seed(UserContext context)
+        {
+            base.Seed(context);
+        }
     }
 }
